93cab2afba8a9489929e96810d176df5330b1959
Andre Rosot <alrosot@yahoo.com.br>
2017-03-04 11:43:18 +1100

    Initial import from start.spring.io

---
+++ src/main/java/br/com/trofo/RaspberryCarLogApplication.java
@@ -0,0 +1,12 @@
+package br.com.trofo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberryCarLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspberryCarLogApplication.class, args);
+	}
+}
